0108abe34ffa33180c51b2c3d9460cacf341eb49
Tanzeem Siddique <t_siddique@mail.fhsu.edu>
2026-04-02 18:50:42 -0500

    Setup React app with Vite, Tailwind, and DaisyUI

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-2xl font-bold text-red-500">
+        Welcome to Multi-Persona Planner
+      </h1>
+    </>
+  );
+}
+
+export default App;
